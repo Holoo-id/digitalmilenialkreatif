ec6afb649e310ce75943d511ed04357bef2f84a4
Andika .R <andika15ramdani@gmail.com>
2021-08-01 12:45:49 +0700

    20210801 - 1245

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './styles/App.sass'
+import About from './components/About'
 import BannerSlider from './components/BannerSlider'
-import bgImage2 from './styles/images/bg-2.jpg'
 function App() {
   return (
-    <div className="App" style={{
-      background: `url(${bgImage2}) no-repeat center center fixed`
-    }}>
+    <div className="App">
       <BannerSlider />
+      <About />
     </div>
   );
 }
--- src/components/BannerSlider.js
+++ src/components/BannerSlider.js
@@ -1,9 +1,59 @@
-import React, { useState, useEffect } from 'react'
+// import React, { useState, useEffect } from 'react'
+import Slider from 'infinite-react-carousel'
+import bgImage2 from '../styles/images/bg-2.jpg'
 
 function BannerSlider() {
+  const settings =  {
+    adaptiveHeight: true,
+    arrowsBlock: false,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    dots: true,
+    slidesToShow: 3.5
+  }
+
   return(
     <div className="bannerSlider">
-      
+      <figure className="theBackground"><img src={bgImage2}/></figure>
+      <Slider { ...settings }>
+        <div>
+          <div className="bannerCard">
+            <div className="bcContent"></div>
+            <div className="bcFooter"></div>
+          </div>
+        </div>
+        <div>
+          <div className="bannerCard">
+            <div className="bcContent"></div>
+            <div className="bcFooter"></div>
+          </div>
+        </div>
+        <div>
+          <div className="bannerCard">
+            <div className="bcContent"></div>
+            <div className="bcFooter"></div>
+          </div>
+        </div>
+        <div>
+          <div className="bannerCard">
+            <div className="bcContent"></div>
+            <div className="bcFooter"></div>
+          </div>
+        </div>
+        <div>
+          <div className="bannerCard">
+            <div className="bcContent"></div>
+            <div className="bcFooter"></div>
+          </div>
+        </div>
+        <div>
+          <div className="bannerCard">
+            <div className="bcContent"></div>
+            <div className="bcFooter"></div>
+          </div>
+        </div>
+      </Slider>
     </div>
   );
 }
